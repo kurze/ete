9e556911ab20add45958a996d73b918f28995468
Simon Kesteloot <simon.kesteloot@trustelem.com>
2017-11-29 08:23:13 +0100

    feat(server): add ID to each Request

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
+	"math/rand"
+
 	"github.com/kataras/golog"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
@@ -13,17 +16,29 @@ type server struct {
 	Config     *config
 }
 
+const keyRequestID = "reqID"
+
 func newServer(config *config) *server {
 	app := iris.New()
 	app.Use(recover.New())
-	app.Use(logger.New())
+	app.Use(logger.New(logger.Config{
+		Status:            true,
+		IP:                true,
+		Method:            true,
+		Path:              true,
+		MessageContextKey: keyRequestID,
+	}))
 	app.Logger().SetLevel(config.LogLevel)
 
-	return &server{
+	s := server{
 		Logger:     app.Logger(),
 		HTTPServer: app,
 		Config:     config,
 	}
+
+	app.Use(s.before)
+
+	return &s
 }
 
 func (s *server) run() {
@@ -40,4 +55,25 @@ func (s *server) registerEndpoints() {
 		s.Logger.Debugf("%v %+v", i, c)
 		s.registerCommand(c)
 	}
+}
+
+func (s *server) generateRequestID() string {
+	r := make([]byte, 6)
+	_, err := rand.Read(r)
+	if err != nil {
+		s.Logger.Fatal("According to the lib code, *this* cannot be display.")
+	}
+	return base64.StdEncoding.EncodeToString(r)
+}
+
+func (s *server) before(ctx iris.Context) {
+	ctx.Values().Set(keyRequestID, s.generateRequestID())
+}
+
+func (s *server) getRequestID(ctx iris.Context) string {
+	result, ok := ctx.Values().Get(keyRequestID).(string)
+	if !ok {
+		s.Logger.Fatal("someone do something wrong with requestID")
+	}
+	return result
 }
